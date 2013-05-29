0cfc411d4097f24f14345888b1dce554a1fdfacd
AVVS <v@aminev.me>
2013-05-30 02:25:47 +0400

    fix function naming

--- tasks/eco.js
+++ tasks/eco.js
@@ -91,7 +91,7 @@ module.exports = function(grunt) {
     var basename = path.basename(filepath, ".eco");
 
     try {
-      return require('eco').precompile(code, options).replace(/module\.exports/, "this."+options.namespace+"['"+basename+"']");
+      return "this."+options.namespace+"['"+basename+"'] = " + require('eco').precompile(code, options);
     } catch (e) {
       console.error(e);
       grunt.fail.warn('EcoTemplates failed to compile.');
